7e624469426ad2f989494b716b26080f01824430
orz <wryyyyyyyy@gmail.com>
2013-10-31 12:16:53 -0400

    Add basic file parsing functions.

--- overlord/__main__.py
+++ overlord/__main__.py
@@ -3,6 +3,15 @@ import multiprocessing
 import time
 import library
 
+import os
+import random
+import mutagen #audio metadata module
+import mimetypes
+
+
+# Add .m4a as a valid MIME type.
+mimetypes.add_type('audio/mp4', '.m4a')
+
 parser = argparse.ArgumentParser(description="The Radio Robot Overlord")
 parser.add_argument('directories', metavar="DIR", nargs="*", help="One or more directories to watch for changes.")
 
@@ -25,3 +34,14 @@ def main():
         print "Goodbye."
     finally:
         exit()
+
+# uses MIME to determine if a file is audio; based on extension.
+def isAudio(f):
+    return (mimetypes.guess_type(f)[0] and mimetypes.guess_type(f)[0].startswith('audio'))
+
+# prints full pathnames to all valid audio files in 'path'
+def getFiles(path):
+    for root, dirs, files in os.walk(path, followlinks=True):
+        for filename in files:
+            if (isAudio(filename)):
+                print os.path.join(root, filename)
