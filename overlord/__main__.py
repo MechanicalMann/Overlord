94c9bfb91ca9c47d56f12d549e32ba165ec4168b
orz <wryyyyyyyy@gmail.com>
2014-10-30 17:03:18 -0400

    Basic metadata parsing. Does not gracefully handle non-audio files, yet.

--- overlord/__main__.py
+++ overlord/__main__.py
@@ -6,6 +6,7 @@ import library
 import os
 import random
 import mutagen #audio metadata module
+import wave    #because mutagen doesn't do PCM wave files
 import mimetypes
 import collections
 
@@ -38,15 +39,22 @@ def main():
             while t in recently_played:
                 t = random.choice(music)
                 # ok got a track not in the queue
-            print t
+            print t, "\n\t", getMetadata(t), getDuration(t)
             recently_played.append(t)
 
             time.sleep(0.5)
-
     except KeyboardInterrupt:
         print "Goodbye."
     finally:
         exit()
+
+
+
+####
+# utility functions
+####
+
+
 
 # uses MIME to determine if a file is audio; based on extension.
 def isAudio(f):
@@ -61,3 +69,47 @@ def getFiles(path):
                 #print os.path.join(root, filename)
                 list_of_files.append(os.path.join(root, filename))
     return list_of_files
+
+def getMetadata(audio_file):
+    
+    artist = "Unknown Artist"
+    title = "Unknown Title"
+
+    if audio_file.lower().endswith('.wav'):
+        # leave artist as Unknown
+        # the title will be the filename, no extension
+        title = os.path.split(audio_file)[1][:-4]
+    else:
+        audio = mutagen.File(audio_file, easy=True)
+        try:
+            artist = audio["artist"]
+        except KeyError:
+            pass # key isn't defined, roll with it
+        try:
+            title = audio["title"]
+        except KeyError:
+            pass # key isn't defined, roll with it
+    
+    return artist, title
+
+def getDuration(audio_file):
+    ''' Returns the duration of audio_file, in seconds.
+        Retval will be a float, may contain fractional seconds.
+    '''
+
+    # wave files involve MATHS, because mutagen doesn't deal with wave files
+    if audio_file.lower().endswith('.wav'):
+        try:
+            wav_file = wave.open(audio_file, 'r')
+            # nframes / framerate should give time in seconds, 
+            # float conversion is present to ensure fractional seconds exist
+            time_sec = float(wav_file.getnframes()) / wav_file.getframerate()
+            wav_file.close()
+        except:
+            raise
+
+    else:
+        audio = mutagen.File(audio_file, easy=True)    
+        time_sec = audio.info.length
+
+    return time_sec
